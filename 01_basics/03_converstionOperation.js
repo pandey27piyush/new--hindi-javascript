6e3c95c22b35c4c516856a8857bfd613d69f09ce
pandey27piyush <139256056+pandey27piyush@users.noreply.github.com>
2025-04-05 17:39:27 +0000

    conversatin operations

--- 01_basics/03_converstionOperation.js
+++ 01_basics/03_converstionOperation.js
@@ -10,11 +10,34 @@
 // TRUE=1; false
 
 
-let isLoggedIn = 1
+// let isLoggedIn = 1
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 // 1 = true;0 =false
 // "" =false
 // "piyush" = true
+
+// ***********OPRATIONS*****************
+let value =3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+
+let str1 ="hello"
+let str2 ="piyush"
+
+let str3 = str1+str2
+console.log(str3);
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
