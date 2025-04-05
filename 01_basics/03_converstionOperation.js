5c6ea7942bc0d4e77b248b6479fb09278c6afa15
pandey27piyush <139256056+pandey27piyush@users.noreply.github.com>
2025-04-05 16:59:58 +0000

    Converstion 0f dataTypes Operations

---
+++ 01_basics/03_converstionOperation.js
@@ -0,0 +1,20 @@
+// let score = "33"       ("33abcc")
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+// "33"=33
+// "33abc"= NAN(NOT A NUMBER)
+// TRUE=1; false
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 = true;0 =false
+// "" =false
+// "piyush" = true
